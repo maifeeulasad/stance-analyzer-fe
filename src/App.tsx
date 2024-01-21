6f4648a9de3df7bee207ec96a754541485698a06
maifeeulasad <maifeeulasad@gmail.com>
2024-01-21 22:41:20 +0600

    [route,cleanup]:removed base route, removed unnecessary components;

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,12 @@ import {
 
 import { CustomLayout } from './layout/CustomLayout';
 import { LazyLanding } from './component/landing/LazyLanding';
-import { LazyPage2 } from './component/LazyPage2';
-import { LazyPage3 } from './component/LazyPage3';
 
 const App = () => (
-  <BrowserRouter basename="/react-pipeline">
+  <BrowserRouter>
     <CustomLayout>
       <Routes>
         <Route path="/landing" element={<LazyLanding />} />
-        <Route path="/page2" element={<LazyPage2 />} />
-        <Route path="/page3" element={<LazyPage3 />} />
         <Route
           path="*"
           element={<Navigate to="/landing" replace />}
