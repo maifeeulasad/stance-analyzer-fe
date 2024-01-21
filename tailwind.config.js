417acff328afe0d4f6400c5b9de2a165feadaf16
maifeeulasad <maifeeulasad@gmail.com>
2024-01-21 23:02:29 +0600

    [config]: remove old config;

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content:["./src/component/**/*.{ts,tsx}", "./src/layout/*.{ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
